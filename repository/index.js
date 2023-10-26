4ae8c54433b985117135bb723b84d61c15a326ba
Nazhwa <nazhwanursyabrina@gmail.com>
2023-10-26 11:34:51 +0700

    chore menambah biodata

--- repository/index.js
+++ repository/index.js
@@ -2,4 +2,7 @@ console.log("Hallo ini ringkasan git");
 console.log("Hallo ini juaaaaaa");
 console.log("Hallo ini juaaaaaa lagiiiii");
 
-
+const biodata = [
+    nama = "idung",
+    makanan_fav = "permen yupi"
+]
