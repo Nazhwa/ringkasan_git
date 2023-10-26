67b6f7a7ce8f93a79abe2ba7861a13aa00b708f0
Nazhwa <nazhwanursyabrina@gmail.com>
2023-10-26 11:18:06 +0700

    menambah console jua

--- repository/index.js
+++ repository/index.js
@@ -1,8 +1,2 @@
 console.log("Hallo ini ringkasan git");
-console.log("Hallo ini juaaaaaa");
-console.log("Hallo ini juaaaaaa lagiiiii");
-
-const biodata = [
-    nama = "idung",
-    makanan_fav = "permen yupi"
-]
+console.log("Hallo ini jua");
